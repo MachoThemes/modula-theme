02b34dbadf5c42000e3918e3c1e3d6b0bb84b893
cristianraiber <cristian@machothemes.com>
2019-10-02 12:16:54 +0300

    title css changes on pricing page

--- core/filters.php
+++ core/filters.php
@@ -324,6 +324,6 @@ function modula_theme_add_beacon() {
 
     ?>
 		<script type="text/javascript">!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});</script>
-		<script type="text/javascript">window.Beacon('init', 'c40559a2-6acf-4283-96a6-183fa5da758c')</script><style>
+		<script type="text/javascript">window.Beacon('init', 'c40559a2-6acf-4283-96a6-183fa5da758c')</script>
     <?php
 }
