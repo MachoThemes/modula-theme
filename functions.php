f3d7bbcb0744e4ab424458006054b6d15b423bec
cristianraiber <cristian@machothemes.com>
2019-10-21 12:21:55 +0300

    last updated instead of published date

--- functions.php
+++ functions.php
@@ -39,18 +39,3 @@ add_action( 'init', 'jp_disable_billing_details' );
 
 add_filter( 'edd-vat-use-checkout-billing-template', '__return_false' );
 add_filter( 'edd_require_billing_address', '__return_false' );
-
-
-function modu;a_last_updated_date( $content ) {
-	$u_time = get_the_time('U');
-	$u_modified_time = get_the_modified_time('U');
-	if ($u_modified_time >= $u_time + 86400) {
-	$updated_date = get_the_modified_time('F jS, Y');
-	$updated_time = get_the_modified_time('h:i a');
-	$custom_content .= '<p class="last-updated">Last updated on '. esc_html( $updated_date ) . ' at '. esc_html( $updated_time ) .'</p>';
-	}
-
-		$custom_content .= $content;
-		return $custom_content;
-	}
-	add_filter( 'the_content', 'modula_last_updated_date' );
