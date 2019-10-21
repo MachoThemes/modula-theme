829b3d66467241a6b8c69865bc10092adf2f87db
cristianraiber <cristian@machothemes.com>
2019-10-21 12:27:59 +0300

    last updated instead of published date

--- template-parts/layouts/single-default.php
+++ template-parts/layouts/single-default.php
@@ -33,7 +33,17 @@
 								</span>
 
 								<span class="title-single__date">
+									<?php
+									$u_time = get_the_time('U');
+									$u_modified_time = get_the_modified_time('U');
+									if ($u_modified_time >= $u_time + 86400) {
+									echo "<p>Last modified on ";
+									the_modified_time('F jS, Y');
+									echo " at ";
+									the_modified_time();
+									echo "</p> "; }
 
+									?>
 								</span>
 
 								<?php if( get_comments_number() !== '0' ) : ?>
