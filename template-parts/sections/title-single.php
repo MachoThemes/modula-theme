97de27bbb711e79832bb65d0de8e4218cf13ae59
cristianraiber <cristian@machothemes.com>
2019-10-21 12:22:32 +0300

    last updated instead of published date

--- template-parts/sections/title-single.php
+++ template-parts/sections/title-single.php
@@ -28,8 +28,18 @@
 						</span>
 
 						<span class="title-single__date">
-							<?php echo get_the_date(); ?>
-						</span>
+									<?php
+									$u_time = get_the_time('U');
+									$u_modified_time = get_the_modified_time('U');
+									if ($u_modified_time >= $u_time + 86400) {
+									echo "<p>Last modified on ";
+									the_modified_time('F jS, Y');
+									echo " at ";
+									the_modified_time();
+									echo "</p> "; }
+
+									?>
+								</span>
 
 						<?php if( get_comments_number() !== '0' ) : ?>
 							<span class="title-single__comments">
