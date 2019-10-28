45f349eb110ecb0dab043ebf4798534ddee1a00b
cristianraiber <cristian@machothemes.com>
2019-10-28 17:33:06 +0200

    code tweaks

--- core/filters.php
+++ core/filters.php
@@ -382,26 +382,14 @@
 
 	/**
 	 * Clean up WordPress Header
-     *
+	 *
 	 * */
-	function modula_remove_version() {
-		return '';
-	}
-
-	add_filter( 'the_generator', 'modula_remove_version' );
-	remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
-	remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
-	remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
-
-	remove_action( 'wp_head', 'rsd_link' );
-	remove_action( 'wp_head', 'wlwmanifest_link' );
-	remove_action( 'wp_head', 'wp_shortlink_wp_head' );
-
-	function modula_cleanup_query_string( $src ) {
-		$parts = explode( '?', $src );
-
-		return $parts[0];
-	}
-
-	add_filter( 'script_loader_src', 'modula_cleanup_query_string', 15, 1 );
-	add_filter( 'style_loader_src', 'modula_cleanup_query_string', 15, 1 );
+	remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+	remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
+	remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+	remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+	remove_action( 'wp_head', 'index_rel_link' ); // index link
+	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
+	remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+	remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
