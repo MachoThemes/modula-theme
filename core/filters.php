e280b0b2fd059a781b2641607e85ea5462ab13ab
cristianraiber <cristian@machothemes.com>
2019-10-28 17:35:22 +0200

    more head code cleanup

--- core/filters.php
+++ core/filters.php
@@ -393,3 +393,7 @@
 	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
 	remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
 	remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
+	remove_action( 'wp_head', 'wp_shortlink_wp_head');
+	remove_action('wp_head', 'rest_output_link_wp_head', 10);
+	remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+	remove_action('template_redirect', 'rest_output_link_header', 11, 0);
