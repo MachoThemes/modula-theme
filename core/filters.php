e3deb402ef042bf3c825a3b522bd1b245825986f
cristianraiber <cristian@machothemes.com>
2019-10-28 17:31:18 +0200

    clean up head section

--- core/filters.php
+++ core/filters.php
@@ -378,3 +378,30 @@
 
 		return $urls;
 	}
+
+
+	/**
+	 * Clean up WordPress Header
+     *
+	 * */
+	function modula_remove_version() {
+		return '';
+	}
+
+	add_filter( 'the_generator', 'modula_remove_version' );
+	remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+	remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+	remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
+
+	remove_action( 'wp_head', 'rsd_link' );
+	remove_action( 'wp_head', 'wlwmanifest_link' );
+	remove_action( 'wp_head', 'wp_shortlink_wp_head' );
+
+	function modula_cleanup_query_string( $src ) {
+		$parts = explode( '?', $src );
+
+		return $parts[0];
+	}
+
+	add_filter( 'script_loader_src', 'modula_cleanup_query_string', 15, 1 );
+	add_filter( 'style_loader_src', 'modula_cleanup_query_string', 15, 1 );
