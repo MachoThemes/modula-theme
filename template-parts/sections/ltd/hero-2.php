da81eb0131c0c9008d3bbcc46d4595912a39133b
George Ciobanu <george.ciobanu91@gmail.com>
2019-11-20 14:37:55 +0200

    change headline

--- template-parts/sections/ltd/hero-2.php
+++ template-parts/sections/ltd/hero-2.php
@@ -6,12 +6,12 @@
         <div class="row">
 
             <div class="col-md-6 text-center text-md-left" style="z-index:1">
-                <h1 class="mb-3">Empowering photographers, designers, creators and artists to build unique looking galleries</h1>
+                <h1 class="mb-3">Martech Wise Black-friday special deal.</h1>
                 <p class="mb-3">With our drag-to-fit grid system you can take your pictures and drag them into unique layouts that put your images in the spotlight.</p>
                 <p class="mb-3">
                 <div class="stars mr-2"></div><strong>60,000+</strong> Active Users
                 </p>
-                <a class="button button--xl mr-1 mb-md-0" href="<?php echo esc_url( get_permalink( get_page_by_path( 'pricing' ) ) ); ?>">Buy Modula Gallery</a>
+                <a class="button button--xl mr-1 mb-md-0" href="#pricing">Buy Modula Gallery</a>
             </div>
 
             <div class="col-md-6" style="z-index:0;">
