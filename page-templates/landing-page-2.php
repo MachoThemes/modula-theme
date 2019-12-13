5e8e5efc1b6f14c54a8fa569dd04156697826ad0
cristianraiber <cristian@machothemes.com>
2019-12-13 16:18:25 +0200

    moved page builder compatibility to the top of the hero section

--- page-templates/landing-page-2.php
+++ page-templates/landing-page-2.php
@@ -3,12 +3,12 @@
 <?php get_header(); ?>
 
 <?php get_template_part( 'template-parts/sections/hero-2' ); ?>
-<?php get_template_part( 'template-parts/sections/highlights' ); ?>
+<?php get_template_part( 'template-parts/sections/compatibility' ); ?>
+<?php //get_template_part( 'template-parts/sections/highlights' ); ?>
 <?php get_template_part( 'template-parts/sections/services-2' ); ?>
 <?php get_template_part( 'template-parts/sections/stats' ); ?>
 <?php get_template_part( 'template-parts/sections/as-seen-on' ); ?>
 <?php get_template_part( 'template-parts/sections/testimonials-2' ); ?>
-<?php get_template_part( 'template-parts/sections/compatibility' ); ?>
 <?php get_template_part( 'template-parts/sections/faq' ); ?>
 <?php get_template_part( 'template-parts/sections/cta' ); ?>
 <?php get_template_part( 'template-parts/misc/modals/video' ); ?>
