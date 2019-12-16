be12872561e1c7232d23940c26baf473a17c0bc9
cristianraiber <cristian@machothemes.com>
2019-12-16 15:57:27 +0200

    added updated on

--- template-parts/sections/title-single.php
+++ template-parts/sections/title-single.php
@@ -1,27 +1,27 @@
 <?php while ( have_posts() ) : ?>
 	<?php the_post(); ?>
-	<section class="title-single-section">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-12 text-center">
-					<!-- <div class="title-single__cats">
+    <section class="title-single-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    <!-- <div class="title-single__cats">
 						<?php echo wp_kses_post( get_the_category_list( ' ' ) ); ?>
 					</div> -->
-					<h1 class="h2 title-single__title mb-0"><?php echo esc_html( get_the_title() ); ?></h1>
-				</div>
-				<?php if( has_post_thumbnail() ): ?>
-					<div class="col-md-10">
-						<figure class="wp-block-image alignwide mb-0 mt-0">
-							<div class="title-single__thumbnail">
+                    <h1 class="h2 title-single__title mb-0"><?php echo esc_html( get_the_title() ); ?></h1>
+                </div>
+				<?php if ( has_post_thumbnail() ): ?>
+                    <div class="col-md-10">
+                        <figure class="wp-block-image alignwide mb-0 mt-0">
+                            <div class="title-single__thumbnail">
 								<?php echo wp_get_attachment_image( get_post_thumbnail_id(), "full", false ); ?>
-							</div>
-						</figure>
-					</div>
-					<div class="clear"></div>
+                            </div>
+                        </figure>
+                    </div>
+                    <div class="clear"></div>
 				<?php endif; ?>
 
-				<div class="col-md-7">
-					<div class="title-single__meta mb-3">
+                <div class="col-md-7">
+                    <div class="title-single__meta mb-3">
 
 
 
@@ -29,27 +29,28 @@
 							<?php
 
 							$d = get_option( 'date_format' );
-							echo get_post_modified_time( $d );
+							echo esc_html__('Updated on:', 'modula') . get_post_modified_time( $d );
 
 							?>
 						</span>
 
-						<?php if( get_comments_number() !== '0' ) : ?>
+						<?php if ( get_comments_number() !== '0' ) : ?>
                             <span class="title-single__comments">
-								<a title="<?php echo esc_attr__( 'Comment on Post', 'modula' ); ?>" href="<?php echo esc_url( get_the_permalink( get_the_ID() ) ); ?>#comments">
+								<a title="<?php echo esc_attr__( 'Comment on Post', 'modula' ); ?>"
+                                   href="<?php echo esc_url( get_the_permalink( get_the_ID() ) ); ?>#comments">
 									<?php esc_html( comments_number( __( 'no comments', 'modula' ), __( 'one comment', 'modula' ), __( '% comments', 'modula' ) ) ); ?>
 								</a>
 							</span>
 						<?php endif; ?>
 
 
-						<span class="title-single__read">
+                        <span class="title-single__read">
 							<?php echo floor( modula_reading_time( get_the_content() ) / 60 ) + 1; ?> min read
 						</span>
 
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php endwhile; ?>
