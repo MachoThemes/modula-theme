2a7413f28fdd2313cf3674bef56e3ee738b07345
cristianraiber <cristian@machothemes.com>
2020-05-01 13:11:13 +0300

    updated footer copyright message

--- template-parts/sections/footer-simple.php
+++ template-parts/sections/footer-simple.php
@@ -2,7 +2,7 @@
 	<div class="footer container">
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2 text-center">
-				<p class="mb-0">© 2017-<?php echo date("Y"); ?> WP-Modula. All rights reserved. </p>
+				<p class="mb-0">© 2017-<?php echo date("Y"); ?> WP Chill. All rights reserved. A <a href="https://wpchill.com" target="_blank">WP Chill</a> product </p>
 			</div>
 		</div>
 	</div>
