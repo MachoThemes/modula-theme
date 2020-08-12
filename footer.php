6e7354c8dfcbcda75c6d537e9ee15c71f5101bcf
cristianraiber <cristian@machothemes.com>
2020-08-12 10:46:50 +0300

    load helpscout beacon only on homepage & pricing pages

--- footer.php
+++ footer.php
@@ -1,11 +1,28 @@
 <?php get_template_part( 'template-parts/misc/modals/login' ); ?>
 
-<?php do_action('modula_footer');  ?>
+<?php do_action( 'modula_footer' ); ?>
 
 </section><!-- site-wrap -->
 
 <?php wp_footer(); ?>
-<script type="text/javascript">!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});</script>
-<script type="text/javascript">window.Beacon('init', 'c40559a2-6acf-4283-96a6-183fa5da758c')</script>
+<?php if ( is_page( array( 'pricing', 'v2' ) ) ) { ?>
+	<!-- load helpscout beacon below -->
+<script type="text/javascript">!function( e, t, n ) {
+	function a() {
+		var e = t.getElementsByTagName( "script" )[ 0 ], n = t.createElement( "script" );
+		n.type = "text/javascript", n.async = !0, n.src = "https://beacon-v2.helpscout.net", e.parentNode.insertBefore( n, e )
+	}
+
+	if ( e.Beacon = n = function( t, n, a ) {
+		e.Beacon.readyQueue.push( { method: t, options: n, data: a } )
+	}, n.readyQueue = [], "complete" === t.readyState ) {
+		return a();
+	}
+	e.attachEvent ? e.attachEvent( "onload", a ) : e.addEventListener( "load", a, !1 )
+}( window, document, window.Beacon || function() {
+} );</script>
+<script type="text/javascript">window.Beacon( 'init', 'c40559a2-6acf-4283-96a6-183fa5da758c' )</script>
+	<!-- end helpscout beacon -->
+<?php } ?>
 </body>
 </html>
