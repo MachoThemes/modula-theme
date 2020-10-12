8cbc812d893b4163894eb2f7901649c93a078d8c
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2020-10-12 12:03:27 +0300

    added divi to works great with

--- template-parts/sections/compatibility.php
+++ template-parts/sections/compatibility.php
@@ -7,23 +7,28 @@
 			</div>
 		</div>
 
-		<div class="row compatibility-section__images justify-content-center align-items-center">
-			<div class="col-xs-6 col-md-3 mb-3">
+		<div class="row compatibility-section__images justify-content-between align-items-center">
+			<div class="col-xs-6 col-md-2 mb-3">
 				<div class="compatibility-section__img">
 					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/elementor.png' ); ?> " alt="Elementor">
 				</div>
 			</div>
-			<div class="col-xs-6 col-md-3 mb-3">
+			<div class="col-xs-6 col-md-2 mb-3">
 				<div class="compatibility-section__img" style="height:44px;">
 					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/beaver-builder.png' ); ?> " alt="Beaver Builder">
 				</div>
 			</div>
-			<div class="col-xs-6 col-md-3 mb-3">
+			<div class="col-xs-6 col-md-2 mb-3">
+				<div class="compatibility-section__img" style="height:32px;">
+					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/divi.png' ); ?> " alt="Divi Builder">
+				</div>
+			</div>
+			<div class="col-xs-6 col-md-2 mb-3">
 				<div class="compatibility-section__img" style="height:30px;">
 					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/siteorigin.png' ); ?> " alt="SiteOrigin">
 				</div>
 			</div>
-			<div class="col-xs-6 col-md-3 mb-3">
+			<div class="col-xs-6 col-md-2 mb-3">
 				<div class="compatibility-section__img" style="height:32px;">
 					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/gutenberg.png' ); ?> " alt="Gutenberg">
 				</div>
