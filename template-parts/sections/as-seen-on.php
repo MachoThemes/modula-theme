2d0aabc177856899b151c6475046180770ecc2e6
imp <imp@192-168-0-135.rdsnet.ro>
2020-11-18 10:13:53 +0200

    made the FixThePhoto logo actually be a link

--- template-parts/sections/as-seen-on.php
+++ template-parts/sections/as-seen-on.php
@@ -1,74 +1,86 @@
 <section class="as-seen-on-section">
-	<div class="container">
+    <div class="container">
 
-		<div class="row">
-			<div class="section__heading text-center col-md-8 col-md-offset-2">
-				<h3>As Seen On</h3>
-			</div>
-		</div>
+        <div class="row">
+            <div class="section__heading text-center col-md-8 col-md-offset-2">
+                <h3>As Seen On</h3>
+            </div>
+        </div>
 
-		<div class="row as-seen-on-section__images">
-			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
-				<div class="as-seen-on-section__images__img">
-					<a href="https://colorlib.com">
-						<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/colorlib.png' ); ?> " alt="Colorlib">
-					</a>
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
-				<div class="as-seen-on-section__images__img">
-					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/winning-wp.png' ); ?> " alt="Winning WP">
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
-				<div class="as-seen-on-section__images__img" style="height:25px;">
-					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/themeisle.png' ); ?> " alt="ThemeIsle">
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
-				<div class="as-seen-on-section__images__img">
-					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/wp-engine.png' ); ?> " alt="WP Engine">
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
-				<div class="as-seen-on-section__images__img" style="height:40px;">
-					<img src="<?php echo get_template_directory_uri() . '/assets/images/logos/wpbuffs.png'; ?>" alt="WPBuffs">
-				</div>
-			</div>
-			<div class="clear visible-lg"></div>
-			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
-				<div class="as-seen-on-section__images__img" style="height:30px;">
-					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/fixthephoto.png' ); ?> " alt="FixThePhoto">
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
-				<div class="as-seen-on-section__images__img" style="height:40px;">
-					<a href="https://shotkit.com">
-					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/shotkit.png' ); ?> " alt="Shotkit">
-					</a>
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
-				<div class="as-seen-on-section__images__img" style="height:27px;">
-					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/kinsta.png' ); ?> " alt="Kinsta">
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
-				<div class="as-seen-on-section__images__img" style="height:40px;">
-					<a href="https://cpothemes.com/wordpress-photo-gallery-plugins">
-						<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/cpothemes.png' ); ?>" alt="CPOThemes">
-					</a>
-				</div>
-			</div>
+        <div class="row as-seen-on-section__images">
+            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+                <div class="as-seen-on-section__images__img">
+                    <a href="https://colorlib.com">
+                        <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/colorlib.png'); ?> "
+                             alt="Colorlib">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+                <div class="as-seen-on-section__images__img">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/winning-wp.png'); ?> "
+                         alt="Winning WP">
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+                <div class="as-seen-on-section__images__img" style="height:25px;">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/themeisle.png'); ?> "
+                         alt="ThemeIsle">
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+                <div class="as-seen-on-section__images__img">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/wp-engine.png'); ?> "
+                         alt="WP Engine">
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+                <div class="as-seen-on-section__images__img" style="height:40px;">
+                    <img src="<?php echo get_template_directory_uri() . '/assets/images/logos/wpbuffs.png'; ?>"
+                         alt="WPBuffs">
+                </div>
+            </div>
+            <div class="clear visible-lg"></div>
+            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+                <div class="as-seen-on-section__images__img" style="height:30px;">
+                    <a href="https://fixthephoto.com" target="_blank">
+                        <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/fixthephoto.png'); ?> "
+                             alt="FixThePhoto">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+                <div class="as-seen-on-section__images__img" style="height:40px;">
+                    <a href="https://shotkit.com">
+                        <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/shotkit.png'); ?> "
+                             alt="Shotkit">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+                <div class="as-seen-on-section__images__img" style="height:27px;">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/kinsta.png'); ?> "
+                         alt="Kinsta">
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+                <div class="as-seen-on-section__images__img" style="height:40px;">
+                    <a href="https://cpothemes.com/wordpress-photo-gallery-plugins">
+                        <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/cpothemes.png'); ?>"
+                             alt="CPOThemes">
+                    </a>
+                </div>
+            </div>
 
-			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
-				<div class="as-seen-on-section__images__img">
-					<a href="https://www.codeinwp.com">
-						<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/codeinwp.png' ); ?> " alt="CodeInWP">
-					</a>
-				</div>
-			</div>
-		</div>
+            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 mb-3">
+                <div class="as-seen-on-section__images__img">
+                    <a href="https://www.codeinwp.com">
+                        <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logos/codeinwp.png'); ?> "
+                             alt="CodeInWP">
+                    </a>
+                </div>
+            </div>
+        </div>
 
-	</div>
+    </div>
 </section>
