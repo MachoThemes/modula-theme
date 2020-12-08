28f20d263d4cdb74b4ae291d3384da11b5fefcbe
cristian <raibercristian@gmail.com>
2020-12-08 15:44:22 +0200

    slight CTA tweak

--- template-parts/sections/hero-2.php
+++ template-parts/sections/hero-2.php
@@ -9,7 +9,7 @@
                 <h1 class="mb-2">Get a Highly Customizable Gallery Plugin Without Touching a Line of Code</h1>
                 <p class="mb-2">Finally, there’s a way to impress your clients with your amazing pictures and graphics!</p>
                 <p>Modula is fully customizable <u>without touching a single line of code, does not slow down your website and is fully mobile responsive!</u></p>
-                <p class="mb-2"><strong>Migrating images is 100% Free, <u>Import 100s In seconds!</u></strong></p>
+                <p class="mb-2"><strong>Migrating images is 100% free, <u>import 100s in seconds!</u></strong></p>
 
                 <a class="button button--xl mr-1" href="<?php echo esc_url( get_permalink( get_page_by_path( 'pricing' ) ) ); ?>">Get Started Now</a>
                 <div><small class="hero_money_back_guarantee" style="font-size: 13px; position:relative;top: -15px;">14 day money back guarantee, love it or get a full refund.</small></div>
