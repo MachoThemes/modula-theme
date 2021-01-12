41e2914cd07956a6afbac2eec78f9c5bdc83606e
cristian <raibercristian@gmail.com>
2021-01-12 11:29:30 +0200

    small css tweaks to hero section

--- template-parts/sections/hero-2.php
+++ template-parts/sections/hero-2.php
@@ -11,13 +11,14 @@
 						website and is fully mobile responsive!</u></p>
 				<p class="mb-2"><strong>Migrating images is 100% free, <u>import 100s in seconds!</u></strong></p>
 
-                <a class="button button--xl" href="<?php echo esc_url( get_permalink( get_page_by_path( 'pricing' ) ) ); ?>">
-                    Get Started Now with Modula
-                </a>
-                <div class="text-center">
-                    <small class="cta_money_back_guarantee" style="font-size: 13px; position:relative;top: -15px;">
-                14 day money back guarantee, love it or get a full refund.</small>
-                 </div>
+				<a class="button button--xl" href="<?php echo esc_url( get_permalink( get_page_by_path( 'pricing' ) ) ); ?>">
+					Get Started Now with Modula
+				</a>
+				
+					<small class="cta_money_back_guarantee text-center">
+					  14 day money back guarantee, love it or get a full refund.
+					 </small>
+
 			</div>
 
 			<div class="col-md-6 col-lg-offset-1 text-right" style="z-index:0;">
