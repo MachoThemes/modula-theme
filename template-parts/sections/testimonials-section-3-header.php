0815f6f341fef062291645460a7f4f2c920bb72f
Raiber Cristian <raibercristian@gmail.com>
2021-05-11 10:47:53 +0300

    Forgot to close a p tag

--- template-parts/sections/testimonials-section-3-header.php
+++ template-parts/sections/testimonials-section-3-header.php
@@ -6,8 +6,8 @@
                 <?php echo wp_get_attachment_image(462018, "thumbnail", false, array('class' => 'testimonial__avatar mr-3')); ?>
             </div>
             <div class="testimonial__content col-xs-7">
-                Modula is <b>fast</b>, <b>easy to get started</b> and it’s also <b>super
-                    customizable</b>. - Joe Casabona</p>
+                <p>Modula is <b>fast</b>, <b>easy to get started</b> and it’s also <b>super
+                        customizable</b>. - Joe Casabona</p>
             </div><!-- testimonial -->
         </div>
     </div>
