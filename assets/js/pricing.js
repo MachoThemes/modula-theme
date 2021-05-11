c662e64aa8e83f710a1334d9e5d3e8caf7938379
Raiber Cristian <raibercristian@gmail.com>
2021-05-11 11:35:20 +0300

    Fixes for plan comparison tables on old pricing page template

--- assets/js/pricing.js
+++ assets/js/pricing.js
@@ -3,9 +3,9 @@
 	$(document).ready(function(){
 		$('.compare-plans-button').on('click', function(e) {
                     e.preventDefault();
-                    $('.pricing-section').show();
+                    $('.pricing-table--comparison').show();
                     $("body,html").animate(
-                    { scrollTop: $('.pricing-section').offset().top }, 800 );
+                    { scrollTop: $('.pricing-table--comparison').offset().top }, 800 );
 		});
 	});
         
