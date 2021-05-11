a562bdd031cd66d4e98ba5ef654d7f01e1d76e3d
Raiber Cristian <raibercristian@gmail.com>
2021-05-11 12:43:57 +0300

    CSS tweaks for responsive

--- template-parts/sections/testimonials-section-3-faq.php
+++ template-parts/sections/testimonials-section-3-faq.php
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="testimonial__stars"></div>
         <div class="row justify-content-center">
-            <div class="testimonial__author col-xs-1">
+            <div class="testimonial__author col-sm-1">
                 <?php echo wp_get_attachment_image(460459, "thumbnail", false, array('class' => 'testimonial__avatar mr-3')); ?>
             </div>
-            <div class="testimonial__content col-xs-7">
+            <div class="testimonial__content col-sm-7">
                 <p class="mb-0">Modula is <b>an excellent solution for photographers</b> wanting an attractive,
                     feature-packed and easy-to-use gallery to display their work. - Mark Condon</p>
             </div><!-- testimonial -->
--- template-parts/sections/testimonials-section-3-header.php
+++ template-parts/sections/testimonials-section-3-header.php
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="testimonial__stars"></div>
         <div class="row justify-content-center">
-            <div class="testimonial__author col-xs-1">
+            <div class="testimonial__author col-sm-1">
                 <?php echo wp_get_attachment_image(462018, "thumbnail", false, array('class' => 'testimonial__avatar mr-3')); ?>
             </div>
-            <div class="testimonial__content col-xs-7">
+            <div class="testimonial__content col-sm-7">
                 <p class="mb-0">Modula is <b>fast</b>, <b>easy to get started</b> and it’s also <b>super
                         customizable</b>. - Joe Casabona</p>
             </div><!-- testimonial -->
