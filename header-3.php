3a166ffbf2a82b7462425aaba91594e02006da5a
Raiber Cristian <raibercristian@gmail.com>
2021-05-18 13:28:22 +0300

    Added heatmap tracking

--- header-3.php
+++ header-3.php
@@ -3,7 +3,15 @@
 <head>
 	<?php wp_head(); ?>
 	<meta name="theme-color" content="#2ebf91">
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "6qqx789ynq");
+    </script>
     <script src="https://www.googleoptimize.com/optimize.js?id=GTM-TPVPWRK"></script>
+
 </head>
 
 <body <?php body_class(); ?>>
