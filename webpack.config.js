7153789df8d7e9c9b232ebe0767de2506fd9f530
andyluak <49715292+andyluak@users.noreply.github.com>
2021-07-13 12:54:19 +0300

    Npm update

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,51 @@
-const path                      = require('path');
-const OptimizeCssAssetsPlugin   = require('optimize-css-assets-webpack-plugin');
-const CssEntryPlugin            = require('css-entry-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const config = {
-	entry: {
-		index: './src/js/index.js',
-		gutenberg: './src/js/gutenberg.js',
-		style: './src/scss/style.scss',
-	},
-	output: {
-		filename: 'js/[name].js',
-		path: path.resolve(__dirname, 'assets')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.scss$/,
-		/* 		use: ExtractTextPlugin.extract({
+    entry: {
+        index: './src/js/index.js',
+        gutenberg: './src/js/gutenberg.js',
+        style: './src/scss/style.scss',
+    },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, 'assets'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                /* 		use: ExtractTextPlugin.extract({
 					fallback: 'style-loader', */
-				  	use: ['css-loader?url=false', 'postcss-loader', 'sass-loader']
-				//}),
-			},
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				loader: 'babel-loader',
-			}
-		]
-	},
-	plugins: [
-		new CssEntryPlugin({
-			output: {
-			  filename: "/css/[name].css"
-			}
-		})
-	]
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader?url=false',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+                //}),
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '/css/[name].css',
+        }),
+    ],
 };
 
 //If true JS and CSS files will be minified
 if (process.env.NODE_ENV === 'production') {
-	config.plugins.push(
-		//new UglifyJSPlugin(),
-		new OptimizeCssAssetsPlugin()
-	);
+    config.plugins.push(
+        //new UglifyJSPlugin(),
+        new CssMinimizerPlugin()
+    );
 }
 
 module.exports = config;
